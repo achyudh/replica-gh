d2a372b0ef9ead40df6ecaaf599fa2a5bac58f8a
Achyudh Ram <achyudhk@gmail.com>
2018-11-26 16:29:44 -0500

    Update KimCNN API to accept 2 text sequences

--- lib/model/kim_cnn.py
+++ lib/model/kim_cnn.py
@@ -8,10 +8,12 @@ from tensorflow.python.keras.models import Model
 class KimCNN:
     def __init__(self, embedding_map, embedding_dim, tokenizer, max_sequence_len, num_classes, dataset_name):
         self.dataset_name = dataset_name
+        self.num_classes = num_classes
+        # Declaration for KimCNN-based encoder
+        encoder_input = Input(shape=(max_sequence_len,), dtype='int32')
         embedding_layer = Embedding(len(tokenizer.word_index) + 1, embedding_dim, weights=[embedding_map],
                                     input_length=max_sequence_len, trainable=False)
-        sequence_input = Input(shape=(max_sequence_len,), dtype='int32')
-        embedded_sequences = embedding_layer(sequence_input)
+        embedded_sequences = embedding_layer(encoder_input)
 
         l_conv1 = Conv1D(100, 3, activation='relu', padding='same')(embedded_sequences)
         l_pool1 = GlobalMaxPool1D()(l_conv1)
@@ -19,43 +21,50 @@ class KimCNN:
         l_pool2 = GlobalMaxPool1D()(l_conv2)
         l_conv3 = Conv1D(100, 5, activation='relu', padding='same')(embedded_sequences)
         l_pool3 = GlobalMaxPool1D()(l_conv3)
+        l_concat1 = Concatenate()([l_pool1, l_pool2, l_pool3])
+        encoder = Model(encoder_input, l_concat1)
 
-        l_concat = Concatenate()([l_pool1, l_pool2, l_pool3])
-        l_dropout1 = Dropout(0.2)(l_concat)
+        # Similarity classifier using the KimCNN-based encoder
+        sequence_input1 = Input(shape=(max_sequence_len,), dtype='int32')
+        sequence_input2 = Input(shape=(max_sequence_len,), dtype='int32')
+        l_concat2 = Concatenate()([encoder(sequence_input1), encoder(sequence_input2)])
+        l_dense1 = Dense(100, activation='relu')(l_concat2)
+        l_dropout1 = Dropout(0.4)(l_dense1)
         preds = Dense(num_classes, activation='softmax')(l_dropout1)
-        self.model = Model(sequence_input, preds)
+        self.model = Model([sequence_input1, sequence_input2], preds)
+        self.model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+        self.model.summary()
 
-    def train(self, train_x, train_y, evaluate_x, evaluate_y):
+    def train(self, train_x1, train_x2, train_y, evaluate_x1, evaluate_x2, evaluate_y):
         early_stopping_callback = EarlyStopping(patience=5, monitor='val_acc')
         checkpoint_callback = ModelCheckpoint(filepath="data/models/cnn/%s.hdf5" % self.dataset_name,
                                               monitor='val_acc', verbose=1, save_best_only=True)
-        self.model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-        self.model.summary()
-        self.model.fit(train_x, train_y, validation_data=(evaluate_x, evaluate_y), epochs=20, batch_size=64,
-                       callbacks=[early_stopping_callback, checkpoint_callback])
+        self.model.fit([train_x1, train_x2], train_y, validation_data=([evaluate_x1, evaluate_x2], evaluate_y),
+                       epochs=20, batch_size=64, callbacks=[early_stopping_callback, checkpoint_callback])
         self.model.load_weights("data/models/cnn/%s.hdf5" % self.dataset_name)
 
-    def predict(self, predict_x):
-        return self.model.predict(predict_x)
+    def predict(self, predict_x1, predict_x2):
+        return self.model.predict([predict_x1, predict_x2])
 
-    def evaluate(self, evaluate_x, evaluate_y):
-        predict_y = self.predict(evaluate_x).argmax(axis=1)
+    def evaluate(self, evaluate_x1, evaluate_x2, evaluate_y):
+        predict_y = self.predict(evaluate_x1, evaluate_x2).argmax(axis=1)
         evaluate_y = evaluate_y.argmax(axis=1)
         return {"individual": precision_recall_fscore_support(evaluate_y, predict_y),
                 "micro-average": precision_recall_fscore_support(evaluate_y, predict_y, average="micro")}
 
-    def cross_val(self, data_x, data_y, num_classes, n_splits=5):
+    def cross_val(self, data_x1, data_x2, data_y, n_splits=5):
         skf = StratifiedKFold(n_splits, shuffle=False, random_state=157)
         print("Performing cross validation (%d-fold)..." % n_splits)
         iteration = 1
         mean_accuracy = 0
-        recall_list = [0 for _ in range(num_classes)]
-        precision_list = [0 for _ in range(num_classes)]
-        for train_index, test_index in skf.split(data_x, data_y.argmax(axis=1)):
+        recall_list = [0 for _ in range(self.num_classes)]
+        precision_list = [0 for _ in range(self.num_classes)]
+        for train_index, test_index in skf.split(data_x1, data_y.argmax(axis=1)):
             print("Iteration %d of %d" % (iteration, n_splits))
             iteration += 1
-            self.train(data_x[train_index], data_y[train_index], data_x[test_index], data_y[test_index])
-            metrics = self.evaluate(data_x[test_index], data_y[test_index])
+            self.train(data_x1[train_index], data_x2[train_index], data_y[train_index],
+                       data_x1[test_index], data_x2[test_index], data_y[test_index])
+            metrics = self.evaluate(data_x1[test_index], data_x2[test_index], data_y[test_index])
             precision_list = [x + y for x, y in zip(metrics['individual'][0], precision_list)]
             recall_list = [x + y for x, y in zip(metrics['individual'][1], recall_list)]
             mean_accuracy += metrics['micro-average'][0]
