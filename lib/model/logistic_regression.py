2f62195f5cf78ae9d2ad598c318f15a97b5b9d22
Achyudh Ram <achyudhk@gmail.com>
2018-11-26 03:40:24 -0500

    Fix params in model/logistic_regression

--- lib/model/logistic_regression.py
+++ lib/model/logistic_regression.py
@@ -1,10 +1,13 @@
+import numpy as np
 from nltk import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
-from sklearn.feature_extraction.text import TfidfVectorizer
+from scipy import sparse
 from sklearn import linear_model
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import precision_recall_fscore_support
 from sklearn.model_selection import StratifiedKFold
+from sklearn.ensemble import RandomForestClassifier
 
 english_stopwords = stopwords.words("english")
 
@@ -20,31 +23,39 @@ class LogisticRegression:
     def __init__(self):
         self.vectorizer = TfidfVectorizer(stop_words=english_stopwords, tokenizer=tokenize)
         self.classifier = linear_model.LogisticRegression(random_state=37)
+        # self.classifier = RandomForestClassifier(n_jobs=6, n_estimators=500, random_state=37)
 
-    def train(self, train_x, train_y):
-        train_x = self.vectorizer.fit_transform(train_x)
+    def train(self, train_x1, train_x2, train_y):
+        self.vectorizer.fit(np.concatenate((train_x1, train_x2), axis=0))
+        train_x1 = self.vectorizer.transform(train_x1)
+        train_x2 = self.vectorizer.transform(train_x2)
+        train_x = sparse.hstack((train_x1, train_x2)).A
         self.classifier.fit(train_x, train_y)
 
-    def predict(self, predict_x):
-        predict_x = self.vectorizer.transform(predict_x)
+    def predict(self, predict_x1, predict_x2):
+        predict_x1 = self.vectorizer.transform(predict_x1)
+        predict_x2 = self.vectorizer.transform(predict_x2)
+        predict_x = sparse.hstack((predict_x1, predict_x2)).A
         return self.classifier.predict(predict_x)
 
-    def evaluate(self,  evaluate_x, evaluate_y):
-        evaluate_x = self.vectorizer.transform(evaluate_x)
+    def evaluate(self, evaluate_x1, evaluate_x2, evaluate_y):
+        evaluate_x1 = self.vectorizer.transform(evaluate_x1)
+        evaluate_x2 = self.vectorizer.transform(evaluate_x2)
+        evaluate_x = sparse.hstack((evaluate_x1, evaluate_x2)).A
         predict_y = self.classifier.predict(evaluate_x)
         return {"individual": precision_recall_fscore_support(evaluate_y, predict_y),
                 "micro-average": precision_recall_fscore_support(evaluate_y, predict_y, average="micro")}
 
-    def cross_val(self, data_x, data_y, num_classes, n_splits=5):
-        skf = StratifiedKFold(n_splits, shuffle=True,random_state=157)
+    def cross_validate(self, data_x1, data_x2, data_y, num_classes, n_splits=5):
+        skf = StratifiedKFold(n_splits, shuffle=False, random_state=157)
         print("Performing cross validation (%d-fold)..." % n_splits)
-        precision_list = [0 for i in range(num_classes)]
-        recall_list = [0 for i in range(num_classes)]
-        f1_list = [0 for i in range(num_classes)]
+        precision_list = [0 for _ in range(num_classes)]
+        recall_list = [0 for _ in range(num_classes)]
+        f1_list = [0 for _ in range(num_classes)]
         mean_accuracy = 0
-        for train_index, test_index in skf.split(data_x, data_y):
-            self.train(data_x[train_index], data_y[train_index])
-            metrics = self.evaluate(data_x[test_index], data_y[test_index])
+        for train_index, test_index in skf.split(data_x1, data_y):
+            self.train(data_x1[train_index], data_x2[train_index], data_y[train_index])
+            metrics = self.evaluate(data_x1[test_index], data_x2[test_index], data_y[test_index])
             precision_list = [x + y for x, y in zip(metrics['individual'][0], precision_list)]
             recall_list = [x + y for x, y in zip(metrics['individual'][1], recall_list)]
             f1_list = [x + y for x, y in zip(metrics['individual'][2], f1_list)]
