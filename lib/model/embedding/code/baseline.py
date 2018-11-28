daf6a57f895a85f4c3e310b35e2556cf6f58dcf3
Achyudh Ram <achyudhk@gmail.com>
2018-11-28 00:09:17 -0500

    Add baseline code embedding module

---
+++ lib/model/embedding/code/baseline.py
@@ -0,0 +1,28 @@
+import gensim
+
+from lib.data import fetch
+from lib.util import preprocessing
+
+
+def train(input_path, language, size=300, min_count=20):
+    language_extension_map = {'python': '.py', 'java': '.java'}
+    code_repository = fetch.repositories(input_path, filter_extension=language_extension_map[language])
+    tokenized_repository = list()
+    for blob in code_repository:
+        try:
+            tokenized_repository.append(preprocessing.tokenize_code(blob, language))
+        except SyntaxError:
+            print('SyntaxError:', ' '.join(blob[:50].split()))
+    print('Size of code repository:', len(code_repository))
+    print("Training baseline code embeddings...")
+    model = gensim.models.Word2Vec(tokenized_repository, size=size, workers=8, min_count=min_count)
+    model.save('data/embeddings/code/baseline_size%s_min%s' % (size, min_count))
+    return model
+
+
+def load():
+    pass
+
+
+def embedding_matrix():
+    pass
--- lib/model/embedding/word/gensim.py
+++ lib/model/embedding/word/gensim.py
@@ -5,23 +5,25 @@ import gensim
 def train(x, size=100, min_count=5):
     """
     Train a Word2Vec model from scratch with Gensim
+    :param min_count:
+    :param size:
     :param x: A list of tokenized texts (i.e. list of lists of tokens)
     :return: A trained Word2Vec model
     """
     print("Training Word2Vec...")
     model = gensim.models.Word2Vec(x, size=size, workers=8, min_count=min_count)
-    model.save('data/embedding/word2vec/gensim_size%s_min%s' % (size, min_count))
+    model.save('data/embeddings/word/gensim_size%s_min%s' % (size, min_count))
     return model
 
 
-def load(model_path='data/embedding/word2vec/googlenews_size300.bin', binary=True):
+def load(model_path='data/embeddings/word/googlenews_size300.bin', binary=True):
     if binary:
         return gensim.models.KeyedVectors.load_word2vec_format(model_path, binary=True)
     else:
         return gensim.models.Word2Vec.load(model_path)
 
 
-def embedding_matrix(word_index, model_path='data/embedding/word2vec/googlenews_size300.bin', binary=True):
+def embedding_matrix(word_index, model_path='data/embeddings/word/googlenews_size300.bin', binary=True):
     if binary:
         size = int(model_path.split('.')[-2].split('/')[-1].split('_')[1][4:])
     else:
